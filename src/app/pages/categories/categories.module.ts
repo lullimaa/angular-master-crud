2cb4df2174a3d76e3b37c7cec7b1b7d9b33bc802
pdlucianalima <luciana.lima@cuboconnect.com.br>
2020-06-15 19:23:29 -0300

    colocando form

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,15 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryFormComponent } from './category-form/category-form.component';
 import { CategoryListComponent } from './category-list/category-list.component';
 
-
-
 @NgModule({
   imports: [
     CommonModule,
-    CategoriesRoutingModule
+    CategoriesRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [CategoryListComponent, CategoryFormComponent]
 })
